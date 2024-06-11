d9eed9af6a9b3b4127edce6d80239c9dfd9d6cee
rivka-yuliia <juliacreadora@gmail.com>
2024-06-11 14:37:09 +0300

    splash settings in Scrapy project

--- app/wikipedia/spiders/wiki_splash.py
+++ app/wikipedia/spiders/wiki_splash.py
@@ -4,7 +4,7 @@ import scrapy
 class WikiSplashSpider(scrapy.Spider):
     name = "wiki_splash"
     allowed_domains = ["www.wikipedia.org"]
-    start_urls = ["https://www.wikipedia.org"]
+    start_urls = ["https://en.wikipedia.org/wiki/Web_scraping"]
 
     def parse(self, response, **kwargs):
         pass
